1a877fd170160480dc3591951c6aff7da335f96c
Hinrik <hinrik.helga@gmail.com>
2017-10-13 15:40:58 +0000

    Testing step 1. Returning the sum of two numbers.

--- src/main/java/is/ru/StringCalculator.java
+++ src/main/java/is/ru/StringCalculator.java
@@ -7,9 +7,25 @@ public class StringCalculator {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
+
+		else if (numbers.contains(",")) {
+			String splitNumbers[] = numbers.split(",");
+			return sum(splitNumbers);
+		}
 		
 		else {
 			return 1;
 		}
+	}
+
+	private static int sum(String [] splitNumbers) {
+
+
+		int sumOfNum = makeInt(splitNumbers[0]) + makeInt(splitNumbers[1]);
+		return sumOfNum;
+	}
+
+	private static int makeInt(String number) {
+		return Integer.parseInt("number");
 	}
 }
--- src/test/java/is/ru/StringCalculatorTest.java
+++ src/test/java/is/ru/StringCalculatorTest.java
@@ -13,4 +13,8 @@ public class StringCalculatorTest {
 	public void testOneNumber() {
 		assertEquals(1, StringCalculator.add("1"));
 	}
+
+	public void testTwoNumbers() {
+		assertEquals(3, StringCalculator.add("1,2"));
+	}
 }
